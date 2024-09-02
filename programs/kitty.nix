1d4bab16a67c64699b3dc63e618d9fd72056344a
Square-Face <sq8.square.face@gmail.com>
2024-09-02 21:03:26 +0200

    feat(kitty-bg): made kitty use the tokyonight wallpaper as background

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -6,8 +6,11 @@
         enable = true;
         settings = {
             font_family = "Fira Code Nerd Font";
-            background_opacity = "0.8";
+            background_tint = "0.9";
+            background_color = "#000000";
             background_blur = "10";
+            background_image = "~/.config/home-manager/assets/tokyonight.png";
+            background_image_layout = "cscaled";
             enable_audio_bell = "no";
         };
     };
