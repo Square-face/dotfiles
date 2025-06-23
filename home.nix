1cecf06a97d800f5bc290eb6be962b2f8b867928
Linus Michelsson <linus@sq8.dev>
2025-06-23 14:20:56 +0200

    chore: moved spotify to home.nix and formated

--- home.nix
+++ home.nix
@@ -1,70 +1,71 @@
 { pkgs, ... }:
 
 let
-    # Assuming home.nix and starship.toml are in the same directory
-    hmDir = builtins.toString ./.;
+  # Assuming home.nix and starship.toml are in the same directory
+  hmDir = builtins.toString ./.;
 in
 {
-    imports = if builtins.pathExists ./secrets.nix then
+  imports =
+    if builtins.pathExists ./secrets.nix then
       [
         ./shell.nix
-        ./unfree.nix
         ./ui.nix
         ./secrets.nix
       ]
     else
       throw "secrets.nix is missing. Please read the README on how to create it";
 
-    home.packages = with pkgs; [
-        # Desktop Apps
-        kitty
-        vesktop
-        firefox
-        thunderbird
-        playerctl
+  home.packages = with pkgs; [
+    # Desktop Apps
+    kitty
+    vesktop
+    firefox
+    thunderbird
+    playerctl
+    spotifywm # spotify with proper window manager support
 
-        # LSPs
-        nil
+    # LSPs
+    nil
 
-        # Utils
-        fd
-        file
-        wget
-        ripgrep
-        ripgrep-all
-        libqalculate
-        dig
-        nmap
-        dust
-        fastfetch
+    # Utils
+    fd
+    file
+    wget
+    ripgrep
+    ripgrep-all
+    libqalculate
+    dig
+    nmap
+    dust
+    fastfetch
 
-        grim
-        slurp
-        wl-clipboard-rs
+    grim
+    slurp
+    wl-clipboard-rs
 
-        element-desktop
+    element-desktop
 
-        # Virtualization tools
-        virt-manager
-        qemu
+    # Virtualization tools
+    virt-manager
+    qemu
 
-        # games
-        prismlauncher
-    ];
+    # games
+    prismlauncher
+  ];
 
-    # Configuration files in plaintext
-    home.file = {
-        ".config/starship.toml".source    = "${hmDir}/files/starship.toml";
-        ".config/kitty/kitty.conf".source = "${hmDir}/files/kitty.conf";
+  # Configuration files in plaintext
+  home.file = {
+    ".config/starship.toml".source = "${hmDir}/files/starship.toml";
+    ".config/kitty/kitty.conf".source = "${hmDir}/files/kitty.conf";
 
-        ".config/waybar/config.jsonc".source = "${hmDir}/files/waybar.jsonc";
-        ".config/waybar/lock.sh".source      = "${hmDir}/files/lock.sh";
-        ".config/waybar/style.css".source    = "${hmDir}/files/waybar.css";
+    ".config/waybar/config.jsonc".source = "${hmDir}/files/waybar.jsonc";
+    ".config/waybar/lock.sh".source = "${hmDir}/files/lock.sh";
+    ".config/waybar/style.css".source = "${hmDir}/files/waybar.css";
 
-        ".config/wofi/config".source    = "${hmDir}/files/wofi.conf";
-        ".config/wofi/style.css".source = "${hmDir}/files/wofi.css";
-    };
+    ".config/wofi/config".source = "${hmDir}/files/wofi.conf";
+    ".config/wofi/style.css".source = "${hmDir}/files/wofi.css";
+  };
 
-    programs.home-manager.enable = true; # Let Home Manager install and manage itself.
-    home.stateVersion = "25.05"; # DON'T CHANGE WITHOUT READING NIXOS DOCS
+  programs.home-manager.enable = true; # Let Home Manager install and manage itself.
+  home.stateVersion = "25.05"; # DON'T CHANGE WITHOUT READING NIXOS DOCS
 }
