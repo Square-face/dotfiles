6694308fb70ad9fb5672993f497323e4dec8c479
Linus Michelsson <linus@sq8.dev>
2025-07-16 18:07:31 +0200

    feat: audio & backlight keybinds and backlight waybar module

--- ui.nix
+++ ui.nix
@@ -8,6 +8,7 @@
 {
   home.packages = with pkgs; [
     hellwal
+    brightnessctl
 
     noto-fonts
     noto-fonts-cjk-sans
@@ -160,6 +161,12 @@
           "XF86AudioNext" = "exec playerctl next";
           "XF86AudioPrev" = "exec playerctl previous";
           "XF86AudioPlay" = "exec playerctl play-pause";
+          "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          "XF86AudioMicMute" =  "exec wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+";
+          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-";
+          "XF86MonBrightnessDown" = "exec brightnessctl set 10%-";
+          "XF86MonBrightnessUp" = "exec brightnessctl set 10%+";
         };
       workspaceOutputAssign = [
         {
