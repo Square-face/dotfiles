6f2d22bc102645c79343a1f6188888f35fb3d309
Linus Michelsson <linus@sq8.dev>
2025-09-04 12:33:28 +0200

    feat: made idle-inhibitors work for sway-idle

--- modules/shell/xdg.nix
+++ modules/shell/xdg.nix
@@ -1,17 +1,28 @@
 { pkgs, ... }:
 {
-  xdg = {
-    enable = true;
-    portal = {
-      enable = true;
-      config.sway.default = [
-        "wlr"
-        "gtk"
-      ];
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
+    xdg = {
+        enable = true;
+        portal = {
+                enable = true;
+                config.sway.default = [
+                "wlr"
+                "gtk"
+                ];
+                config.sway = {
+                    "org.freedesktop.impl.portal.ScreenCast" = [
+                        "wlr"
+                    ];
+                    "org.freedesktop.impl.portal.Screenshot" = [
+                        "wlr"
+                    ];
+                    "org.freedesktop.impl.portal.Inhibit" = [
+                        "none"
+                    ];
+                };
+                extraPortals = with pkgs; [
+                    xdg-desktop-portal-wlr
+                    xdg-desktop-portal-gtk
+                ];
+        };
     };
-  };
 }
