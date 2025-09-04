dffdc6b381f322c3bdedf406847f44827f6c4c1e
Linus Michelsson <linus@sq8.dev>
2025-09-04 15:20:33 +0200

    feat: rofi pwetty

--- modules/ui/rofi.nix
+++ modules/ui/rofi.nix
@@ -80,6 +80,104 @@
                 background-color = mkLiteral "transparent";
                 text-color = mkLiteral "inherit";
             };
+
+            listview = {
+                enabled = mkLiteral "true";
+
+                columns = mkLiteral "5";
+                lines = mkLiteral "3";
+
+                cycle = mkLiteral "true";
+                dynamic = mkLiteral "true";
+                scrollbar = mkLiteral "false";
+
+                layout = mkLiteral "vertical";
+                reverse = mkLiteral "false";
+
+                fixed-height = mkLiteral "true";
+                fixed-columns = mkLiteral "true";
+
+                spacing = mkLiteral "0px";
+                margin = mkLiteral "0px";
+                padding = mkLiteral "0px";
+                border = mkLiteral "0px solid";
+                border-radius = mkLiteral "0px";
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "#cdd6f4";
+                cursor = "default"; 
+            };
+
+            scrollbar = {
+                handle-width = mkLiteral "5px";
+                handle-color = mkLiteral "#6c7086";
+                border-radius = mkLiteral "0px";
+                background-color = mkLiteral "#1e1e2e";
+            };
+
+            element = {
+                enabled = mkLiteral "true";
+
+                spacing = mkLiteral "15px";
+                margin = mkLiteral "0px";
+                padding = mkLiteral "20px 10px";
+
+                border = mkLiteral "0px solid";
+                border-radius = mkLiteral "10px";
+
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "#cdd6f4";
+
+                orientation = mkLiteral "vertical";
+                cursor = mkLiteral "pointer"; 
+            };
+
+            "element normal.normal" = {
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "#cdd6f4";
+            };
+
+            "element selected.normal" = {
+                background-color = mkLiteral "white / 5%";
+                text-color = mkLiteral "#cdd6f4";
+            };
+
+            element-icon = {
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "inherit";
+
+                size = mkLiteral "64px";
+                cursor = mkLiteral "inherit"; 
+            };
+
+            element-text = {
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "inherit";
+
+                highlight = mkLiteral "inherit";
+                cursor = mkLiteral "inherit"; 
+
+                vertical-align = mkLiteral "0.5";
+                horizontal-align = mkLiteral "0.5";
+            };
+
+            error-message = {
+                padding = mkLiteral "15px";
+                border = mkLiteral "2px solid";
+                border-radius = mkLiteral "10px";
+                border-color = mkLiteral "#9399b2";
+
+                background-color = mkLiteral "black / 10%";
+                text-color = mkLiteral "#cdd6f4";
+            };
+
+            textbox = {
+                vertical-align = mkLiteral "0.5";
+                horizontal-align = mkLiteral "0.0";
+                highlight = mkLiteral "none";
+
+                background-color = mkLiteral "transparent";
+                text-color = mkLiteral "#cdd6f4";
+            };
         };
     };
 }
