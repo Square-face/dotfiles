71727989c5dcd266afdab06e3e3d771abedcea21
Linus Michelsson <linus@sq8.dev>
2025-09-10 22:54:44 +0200

    feat: grub theme :)

--- modules/bootloader/grub.nix
+++ modules/bootloader/grub.nix
@@ -1,12 +1,17 @@
-{
+{pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+        catppuccin-grub
+    ];
 
-  boot.loader.timeout = 10;
-  boot.loader.grub = {
-    enable = true;
-    useOSProber = true;
-    memtest86.enable = true;
-    efiSupport = true;
-    device = "nodev";
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.timeout = 10;
+    boot.loader.grub = {
+        enable = true;
+        useOSProber = true;
+        memtest86.enable = true;
+        efiSupport = true;
+        device = "nodev";
+        theme = "${pkgs.catppuccin-grub}";
+        splashImage = null;
+    };
+    boot.loader.efi.canTouchEfiVariables = true;
 }
