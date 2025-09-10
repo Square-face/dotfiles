2a80c87ce0b306534832dc314d6bb468196c16ff
Linus Michelsson <linus@sq8.dev>
2025-09-11 01:27:45 +0200

    feat: fixed kanshi config

--- modules/ui/kanshi.nix
+++ modules/ui/kanshi.nix
@@ -1,25 +1,6 @@
-{...}: {
+{config, ...}: {
     services.kanshi.enable = true;
-
     services.kanshi.settings = [
-        {include = "config.d/*";}
-        {
-            profile.name = "PC";
-            profile.outputs = [
-                {
-                    criteria = "Lenovo Group Limited 0x1144 VM-06485";
-                    transform = "90";
-                    position = "2560,600";
-                }
-                {
-                    criteria = "Samsung Electric Company S24D340 0x30343238";
-                    position="640,0";
-                }
-                {
-                    criteria = "Philips Consumer Electronics Company Philips 272P4 AU41344000763";
-                    position="0,1080";
-                }
-            ];
-        }
+        {include = "${config.xdg.configHome}/kanshi/config.d/*";}
     ];
 }
